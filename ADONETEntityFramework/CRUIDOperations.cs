2025108a3197bea43ddf8db59d02826fefcc2570
Nikhil <nikhilbanuri4@gmail.com>
2020-12-17 23:23:27 +0530

    Create CRUIDOperations.cs

---
+++ ADONETEntityFramework/CRUIDOperations.cs
@@ -0,0 +1,31 @@
+﻿using ADONETEntityFramework.SQL_MODELS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADONETEntityFramework
+{
+    public class CRUIDOperations
+    {
+        public void Read()
+        {
+            using (CompanyContext context = new CompanyContext())
+            {
+                //var employees = context.Customers.
+            }
+        }
+
+        public void Insert()
+        {
+            
+            
+            using (CompanyContext context = new CompanyContext())
+            {
+                Product product = new Product()
+                {
+                    ProductName = 
+                }
+            }
+        }
+    }
+}
